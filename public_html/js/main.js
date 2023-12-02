9cd95c8792a5561cfb127f3a3ae0c61433ce8988
spyrosbanos <spyrosb99@gmail.com>
2023-12-02 11:53:31 +0200

    jss sample

---
+++ public_html/js/main.js
@@ -0,0 +1,6 @@
+   function myFunction() {
+          document.getElementById("start").style.fontSize = "25px"; 
+          document.getElementById("start").style.color = "red";
+          document.getElementById("start").style.backgroundColor = "yellow"; 
+     }
+
