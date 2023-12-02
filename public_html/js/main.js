1abdc2bc02761239e3811d7f589098c5c5fb4cd0
spyrosbanos <spyrosb99@gmail.com>
2023-12-02 11:55:56 +0200

    js sample

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,6 +1,6 @@
    function myFunction() {
           document.getElementById("start").style.fontSize = "25px"; 
           document.getElementById("start").style.color = "red";
-          document.getElementById("start").style.backgroundColor = "yellow"; 
+          document.getElementById("start").style.backgroundColor = "blue"; 
      }
 
